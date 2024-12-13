8fe5249c8d490fde168b14a788bc3fae3f0a1101
Ajinkya Dudhal <dudhalajinkya@gmail.com>
2024-12-13 23:47:33 +0530

    Create echo.sh

---
+++ 4.Print in different colors/echo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo hello       world
+
+echo "hello       world"
+
+echo -e "\033[0;31m fail message here" # Red color
+echo -e "\033[0;32m success message here" # Green color
+echo -e "\033[0;33m warning message here" # Yellow color
