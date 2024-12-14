de203bd19742d417f6f95f8f353657dcd06e321a
Ajinkya Dudhal <dudhalajinkya@gmail.com>
2024-12-14 12:19:48 +0530

    Create variable.sh

---
+++ 6.User defined variables/variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# User defined variables
+name="Ajinkya"
+age="28"
+echo "my name is $name and my age is $age"
+echo "my name is ${name} and my age is ${age}" # Using variables in curly braces is the best practice
+
+work="prgramm"
+echo "I am $working"
+echo "I am ${work}ing"
+
+echo 'I am ${work}ing' # Strong quote
