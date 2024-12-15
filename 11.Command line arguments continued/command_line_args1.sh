62530b8d153a565e000c1d9900501ff25ad9a984
Ajinkya Dudhal <dudhalajinkya@gmail.com>
2024-12-15 21:29:48 +0530

    Create command_line_args1.sh

---
+++ 11.Command line arguments continued/command_line_args1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo $1
+echo $2
+echo $11 # Will print argument passed at 1st index and then print simply 1 and won't print 11
+echo $12 # Will print argument passed at 1st index and then print simply 1 and won't print 12
+
+echo ${11}
+echo ${12}
