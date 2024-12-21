13935902addf3a3ee8e411dd5b8770b3b0ed28ca
Ajinkya Dudhal <dudhalajinkya@gmail.com>
2024-12-21 11:04:39 +0530

    Create function.sh

---
+++ 19.Function/function.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function install()
+{
+    echo "installation code !"
+}
+
+configuration()
+{
+    echo "configuration code !"
+}
+
+function deploy
+{
+    echo "deploy code !"
+}
+
+install
+configuration
+deploy
