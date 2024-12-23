ba44679d23e52f199c81ec0b41da118a0f5321eb
Ajinkya Dudhal <dudhalajinkya@gmail.com>
2024-12-23 17:52:28 +0530

    Create nested-if-else.sh

---
+++ 32.Nested if else/nested-if-else.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+number=11
+
+if [[ ${number} -gt 10 ]]
+then
+    if [[ ${number} -gt 50 ]]
+    then
+        echo "Number is greater than 50."
+    else
+        echo "Number is between 10 and 50."
+else
+    echo "Number is less than or equal to 10."
+fi
