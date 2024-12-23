aa5269ac265f1d22d20374bc6e9f7027087f541e
Ajinkya Dudhal <dudhalajinkya@gmail.com>
2024-12-23 18:15:13 +0530

    Update if-or-operator.sh

--- 30.OR operator/if-or-operator.sh
+++ 30.OR operator/if-or-operator.sh
@@ -6,3 +6,13 @@ if [[ ${OS_TYPE} == "Linux" || $(UID) -eq 0 ]]
 then
     echo "OS is Linux or user is root."
 fi
+
+# Real life use case
+read -p "do you want to continue (Y/y/yes) " uservalue
+
+if [[ ${uservalue,,} == "y" || ${uservalue,,} == "yes" ]]
+then
+    echo "You want it !"
+else
+    echo "You don't want it !"
+fi
