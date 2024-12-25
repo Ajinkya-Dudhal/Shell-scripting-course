e71d30fb420f6f84047653801426848f7030c47c
Ajinkya Dudhal <dudhalajinkya@gmail.com>
2024-12-25 22:59:16 +0530

    Update commandline-args.sh

--- 39.Difference between $@ and $*/commandline-args.sh
+++ 39.Difference between $@ and $*/commandline-args.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 echo "====== loop one ======"
-for i in "$@"
+for i in "$@"     # Provided argments will be considered separately
 do
     echo ${i}
 done
 
 echo "====== loop two ======"
-for i in "$*"
+for i in $@     # Provided argments will be considered separately
+do
+    echo ${i}
+done
+
+echo "====== loop three ======"
+for i in "$*"     # Provided argments will be considered as single string
+do
+    echo ${i}
+done
+
+echo "====== loop four ======"
+for i in $*     # Provided argments will be considered separately
 do
     echo ${i}
 done
